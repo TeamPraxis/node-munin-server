1355e543f44bbd1a5ad369ef53c1a9f5e3271e1f
Keith Hamasaki <khamasaki@teampraxis.com>
2014-03-26 11:49:12 -1000

    Added the correct mount point to the cgi call.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 // expects static files in /var/www/html/munin and cgi files in /var/www/cgi-bin
 app.use(express.static('/var/www/html/munin'));
 app.get(/^\/munin-cgi\/([^\/]*)\/(.*)/, function (req, res, next) {
-  cgi('/var/www/cgi-bin/' + req.params[0])(req, res, next);
+  cgi('/var/www/cgi-bin/' + req.params[0], { mountPoint: '/munin-cgi/' + req.params[0] })(req, res, next);
 });
 
 var server = http.createServer(app);
