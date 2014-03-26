2586862b50b8bfb299efadcd918d335268bd231d
Keith Hamasaki <khamasaki@teampraxis.com>
2014-03-25 17:20:59 -1000

    Initial commit.

---
+++ server.js
@@ -0,0 +1,16 @@
+var cgi = require('cgi'),
+    express = require('express'),
+    http = require('http');
+
+var app = express();
+
+// expects static files in /var/www/html/munin and cgi files in /var/www/cgi-bin
+app.use(express.static('/var/www/html/munin'));
+app.get(/^\/munin-cgi\/([^\/]*)\/(.*)/, function (req, res, next) {
+  cgi('/var/www/cgi-bin/' + req.params[0])(req, res, next);
+});
+
+var server = http.createServer(app);
+server.listen(3000, function () {
+  console.log('Munin server started.');
+});
